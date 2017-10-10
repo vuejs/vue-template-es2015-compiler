3a29da2a42e184ec3c7c0d18a1e26e61c2e16b94
Evan You <yyx990803@gmail.com>
2017-10-10 16:47:08 -0400

    update buble dep

---
+++ test.js
@@ -0,0 +1,32 @@
+const transpile = require('./index')
+const Vue = require('vue')
+const { compile } = require('vue-template-compiler')
+
+test('should work', () => {
+  const res = compile(`
+    <div>
+      <div>{{ foo }}</div>
+      <div v-for="{ name } in items">{{ name }}</div>
+    </div>
+  `)
+
+  const toFunction = code => {
+    code = transpile(`function render(){${code}}`)
+    code = code.replace(/^function render\(\)\{|\}$/g, '')
+    return new Function(code)
+  }
+
+  const vm = new Vue({
+    data: {
+      foo: 'hello',
+      items: [
+        { name: 'foo' },
+        { name: 'bar' }
+      ]
+    },
+    render: toFunction(res.render),
+    staticRenderFns: res.staticRenderFns.map(toFunction)
+  }).$mount()
+
+  expect(vm.$el.innerHTML).toMatch(`<div>hello</div> <div>foo</div><div>bar</div>`)
+})
